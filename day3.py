dc1f9a1590632eaad2b093c4dde56bfb628b733a
Josh Kruck <joshua.kruck@gmail.com>
2022-12-03 21:41:59 -0600

    implement part 2 of day 3

--- day3.py
+++ day3.py
@@ -2,19 +2,23 @@ import functools
 import operator
 
 
+def find_common_items(bags):
+    return set(set(bags[0]) & set(bags[1]) & set(bags[2])).pop()
+
+
 def find_duplicate_items(bag):
     size = len(bag)
     half = int(size/2)
     return set(set(bag[0:half]) & set(bag[half:size])).pop()
 
 
-def get_item_priortiy(item):
-    ascii =  ord(item)
-    if ascii >= 97:
-        return ascii - 96
+def get_item_priority(item):
+    ordinal = ord(item)
+    if ordinal >= 97:
+        return ordinal - 96
 
-    if ascii >= 65:
-        return ascii - 38
+    if ordinal >= 65:
+        return ordinal - 38
 
     exit(1)
 
@@ -25,6 +29,15 @@ if __name__ == '__main__':
         bags = f.readlines()
     f.close()
 
+    #get the sum of the bag priority
     [dups.append(find_duplicate_items(bag.strip())) for bag in bags]
-    sum_of_priority = functools.reduce(operator.add, map(get_item_priortiy, dups))
+    sum_of_priority = functools.reduce(operator.add, map(get_item_priority, dups))
     print(sum_of_priority)
+
+    bps = []
+    for n in range(0, len(bags), 3):
+        b = bags[n].strip(), bags[n+1].strip(), bags[n+2].strip()
+        bps.append(get_item_priority(find_common_items(b)))
+
+    sum_of_bage_priority = functools.reduce(operator.add, bps)
+    print(sum_of_bage_priority)
