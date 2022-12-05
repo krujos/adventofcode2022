8117a4d9674353b91d469ed797a66d35940f5c5b
Josh Kruck <joshua.kruck@gmail.com>
2022-12-05 14:55:36 -0600

    day 5 part one

---
+++ day5.py
@@ -0,0 +1,30 @@
+def make_containers(lines):
+    ctr = [[],[],[],[],[],[],[],[],[]]
+    for l in reversed(lines):
+        arr = [v for i, v in enumerate(list(l)) if i % 2 != 0]
+        arr = [v for i, v in enumerate(list(arr)) if i % 2 == 0]
+        for i,v in enumerate(list(arr)):
+            if v == " ":
+                continue
+            ctr[i].append(v)
+
+    return ctr
+
+if __name__ == '__main__':
+    with open('input_day5.txt') as f:
+        input_lines = f.read().splitlines()
+    f.close()
+
+    containers = make_containers(input_lines[0:8])
+
+    for line in input_lines[10:]:
+        instruction = line.split()
+        num = int(instruction[1])
+        f = int(instruction[3])-1
+        t = int(instruction[5])-1
+
+        for n in range(num):
+            containers[t].append(containers[f].pop())
+
+    for r in containers:
+        print(r)
