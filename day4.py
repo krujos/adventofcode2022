6372e66cc562bccf41feea74e0e663c98707d07b
Josh Kruck <joshua.kruck@gmail.com>
2022-12-04 20:36:13 -0600

    Part 2

--- day4.py
+++ day4.py
@@ -2,9 +2,12 @@ import functools
 import operator
 
 
+def has_any_overlap(elf1, elf2):
+    if set(elf1) & set(elf2):
+        return True
+    return False
+
 def has_full_overlap(elf1, elf2):
-    print(elf1)
-    print(elf2)
     if set(elf1).issubset(set(elf2)) or set(elf1).issuperset(set(elf2)):
         return True
     return False
@@ -22,26 +25,16 @@ if __name__ == '__main__':
     with open('input_day4.txt') as f:
         pairs = f.read().splitlines()
     f.close()
-    overlaps = 0
+    full_overlaps = 0
+    any_overlaps = 0
 
-    # e1, e2 = "1 - 81, 82 - 82".split(",")
-    # print(has_full_overlap(make_elf(e1), make_elf(e2)))
-    #
-    # e1, e2 = "17 - 97, 96 - 97".split(",")
-    # print(has_full_overlap(make_elf(e1), make_elf(e2)))
-    #
-    # e1, e2 = "5 - 7, 6 - 79".split(",")
-    # print(has_full_overlap(make_elf(e1), make_elf(e2)))
-    #
-    # e1, e2 = "5 - 7, 5 - 7".split(",")
-    # print(has_full_overlap(make_elf(e1), make_elf(e2)))
-    #
-    #
     for pair in pairs:
         e1, e2 = pair.split(",")
         elf1 = make_elf(e1)
         elf2 = make_elf(e2)
         if has_full_overlap(elf1, elf2):
-            overlaps += 1
+            full_overlaps += 1
+        if has_any_overlap(elf1, elf2):
+            any_overlaps += 1
 
-    print(overlaps)
+    print("Full overlap = ",  full_overlaps)
