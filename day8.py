e1185dcf86ec0d0dfbd9bd518f09e433e8497f5d
Josh Kruck <joshua.kruck@gmail.com>
2022-12-09 11:25:15 -0600

    share wip

--- day8.py
+++ day8.py
@@ -1,10 +1,14 @@
 
 def build_grid(buffer):
-  if buffer = None
-
-
-
   grid = list()
+  if buffer == None:
+    grid.append(list("30373"))
+    grid.append(list("25512"))
+    grid.append(list("65332"))
+    grid.append(list("33549"))
+    grid.append(list("35390"))
+    return grid
+  
   [grid.append(list(row)) for row in buffer]
   return grid
 
@@ -13,7 +17,8 @@ def run():
     b = f.read().splitlines()
   f.close()
 
-  grid = build_grid(b)
+  #grid = build_grid(b)
+  grid = build_grid(None)
 
   for row in grid:
     print(row)
